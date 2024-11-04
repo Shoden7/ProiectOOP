ce6f81d7da6c9e9e340c22b2ffe28ea4aeb6d1f4
Shoden7 <stefanzahariuc007@gmail.com>
2024-11-04 23:11:57 +0200

    Added Objects which include Player.h Environment.h and a simple console output of the player class

--- main.cpp
+++ main.cpp
@@ -1,57 +1,34 @@
 #include <iostream>
-#include <array>
 
 #include <Helper.h>
 
+#include "Objects/Player.h"
+#include "Objects/Envroinment.h"
+
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+    Player const player;
+
+    /// Afiseaza direction
+    std::cout << "Player direction: (";
+    for (size_t i = 0; i < player.getDirection().size(); ++i) {
+        std::cout << player.getDirection()[i];
+        if (i < player.getDirection().size() - 1) std::cout << ", ";
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
+    std::cout << ")" << std::endl;
+
+    /// Afiseaza speed
+    std::cout << "Player speed: (";
+    for (size_t i = 0; i < player.getSpeed().size(); ++i) {
+        std::cout << player.getSpeed()[i];
+        if (i < player.getSpeed().size() - 1) std::cout << ", ";
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
+    std::cout << ")" << std::endl;
+
+    /// Afiseaza gravity
+    std::cout << "Player gravity: " << player.getGravity() << std::endl;
+
     Helper helper;
     helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+
     return 0;
 }
