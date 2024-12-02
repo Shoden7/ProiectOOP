eaf66d5817fc7a2e1e3e53f65e980451da29168a
Shoden7 <stefanzahariuc007@gmail.com>
2024-12-03 01:45:50 +0200

    Added Ice.h and Walls.h, which introduces physics such as sliding and hitting a wall

--- cpp/Objects/Envroinment.h
+++ cpp/Objects/Envroinment.h
@@ -4,37 +4,98 @@
 
 #ifndef ENVIRONMENT_H
 #define ENVIRONMENT_H
-#include <array>
 
+#include "../../godot/godot-cpp/include/godot_cpp/core/class_db.hpp"
+#include "../../godot/godot-cpp/gen/include/godot_cpp/variant/utility_functions.hpp"
+#include <iostream>
 
-class Environment {
+using namespace godot;
+
+/**
+ * @class Environment
+ * @brief Represents an individual environment element in the game world.
+ *
+ * This class manages collision states and interacts with Godot's systems for environmental features.
+ */
+class Environment : public Object {
+    GDCLASS(Environment, Object) // Godot class registration
+
 private:
+    /**
+     * @brief Indicates whether the environment is currently colliding.
+     */
     bool isColliding;
 
 public:
-    // Constructor
-    Environment() = default;
-
-    // Getter
-    bool GetCollision() const { return isColliding; };
-};
-
-
-class Walls {
-private:
-    std::array<Environment, 4> coordinates;
-    std::array<Environment, 4> dimensions;
-
-public:
-    // Constructor
-    Walls(const std::array<Environment, 4> &newCoordinates,
-          const std::array<Environment, 4> &newDimensions) : coordinates(newCoordinates), dimensions(newDimensions) {
+    /**
+     * @brief Default constructor for the Environment class.
+     *
+     * Initializes the `isColliding` flag to `false`.
+     */
+    Environment() : isColliding(false) {
     }
 
-    // Getter
-    std::array<Environment, 4> GetCoordinates() const { return coordinates; }
-    std::array<Environment, 4> GetDimensions() const { return dimensions; }
+    /**
+     * @brief Copy constructor for the Environment class.
+     *
+     * Creates a new Environment instance by copying data from another Environment instance.
+     *
+     * @param other The Environment instance to copy from.
+     */
+    Environment(const Environment &other) : isColliding(other.isColliding) {
+    }
+
+    /**
+     * @brief Assignment operator for the Environment class.
+     *
+     * Assigns the data of one Environment instance to another.
+     *
+     * @param other The Environment instance to copy data from.
+     * @return A reference to the updated Environment instance.
+     */
+    Environment &operator=(const Environment &other) {
+        if (this != &other) {
+            // Avoid self-assignment
+            isColliding = other.isColliding;
+        }
+        return *this;
+    }
+
+    /**
+     * @brief Stream insertion operator for the Environment class.
+     *
+     * Outputs the collision state of the Environment instance.
+     *
+     * @param os The output stream.
+     * @param environment The Environment instance to output.
+     * @return A reference to the updated output stream.
+     */
+    friend std::ostream &operator<<(std::ostream &os, const Environment &environment) {
+        os << "Environment(Collision: " << (environment.isColliding ? "true" : "false") << ")";
+        return os;
+    }
+
+    /**
+     * @brief Gets the collision state of the environment.
+     *
+     * @return `true` if the environment is colliding, `false` otherwise.
+     */
+    bool GetCollision() const { return isColliding; }
+
+    /**
+     * @brief Sets the collision state of the environment.
+     *
+     * @param collision The new collision state.
+     */
+    void SetCollision(bool collision) { isColliding = collision; }
+
+    /**
+     * @brief Binds methods to Godot for use in the editor or scripts.
+     */
+    static void _bind_methods() {
+        ClassDB::bind_method(D_METHOD("GetCollision"), &Environment::GetCollision);
+        ClassDB::bind_method(D_METHOD("SetCollision", "collision"), &Environment::SetCollision);
+    }
 };
 
-
-#endif //ENVIRONMENT_H
+#endif // ENVIRONMENT_H
